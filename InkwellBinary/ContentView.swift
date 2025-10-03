07b723a7e4ec6d545f453d21e82f2872f7fd617f
Michael Fluharty <michael.fluharty@mac.com>
2025-10-03 17:27:45 -0500

    1726 added code for settings view clean build no errors

--- InkwellBinary/ContentView.swift
+++ InkwellBinary/ContentView.swift
@@ -56,31 +56,6 @@ struct ContentView: View {
     }
 }
 
-struct SettingsView: View {
-    @Binding var isPresented: Bool
-    
-    var body: some View {
-        NavigationView {
-            ZStack {
-                Color.black
-                    .ignoresSafeArea()
-                
-                Text("Settings coming soon")
-                    .foregroundColor(.white)
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button("Close") {
-                        isPresented = false
-                    }
-                    .foregroundColor(.white)
-                }
-            }
-        }
-    }
-}
-
 #Preview {
     ContentView()
 }
--- SettingsView.swift
+++ SettingsView.swift
@@ -5,3 +5,29 @@
 //  Created by Michael Fluharty on 10/3/25.
 //
 
+import SwiftUI
+
+struct SettingsView: View {
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.black
+                    .ignoresSafeArea()
+                
+                Text("Settings coming soon")
+                    .foregroundColor(.white)
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Close") {
+                        isPresented = false
+                    }
+                    .foregroundColor(.white)
+                }
+            }
+        }
+    }
+}
